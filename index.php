9c28d2896df23b4d149456aa184c2ff5dcde3708
rsevs3 <rsevs3@gmail.com>
2013-02-26 15:20:38 +0800

    Added basic html into script.

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+echo "<html>
+   <head>
+      <title>Aquaponalog</title> 
+   </head>
+   <body>
+   </body>
+</html>
+";
 ?>
